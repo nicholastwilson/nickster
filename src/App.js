795ca79f646675019e6a1a168324b431d23695d2
nicholastwilson <nicholastwilson@hotmail.com>
2023-03-15 17:10:52 -0700

    Refactor toward centralized Pinochle module, add logos, test animations

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-// import logo from './logo.svg';
-import './App.css';
-import qos_png from './qos.png';
-import jod_png from './jod.png';
 import PinochleGame from './games/PinochleGame';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={qos_png} alt="Queen of Spades" className="App-logo" />
-        <img src={jod_png} alt="Jack of Diamonds" className="App-logo2" />
-        <p>Nickster Cards™ coming to a browser near you!</p>
-        <button style={{height: "30px", width: "100px", fontSize: "16px"}} onClick={PinochleGame.start}>Play!</button>
-      </header>
+      <PinochleGame />
     </div>
   );
 }
