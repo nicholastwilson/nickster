1bedd6ac7b06fdcd070699bd26fae8e5fe596f03
nicholastwilson <nicholastwilson@hotmail.com>
2023-03-17 11:20:56 -0700

    Testing react-animations, styled-components, CSS animations

--- src/games/PinochleGame.js
+++ src/games/PinochleGame.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import styled, { keyframes } from 'styled-components';
 import { merge, tada, flip } from 'react-animations';
-import './pinochle.css';
+// import 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'
+import './pinochle-title.css';
 import qos_png from './qos.png';
 import jod_png from './jod.png';
 
@@ -9,23 +10,19 @@ class PinochleGame extends Component {
 
     render() {
         const TadaFlip = styled.div`font-size: 48px; margin-top: 0.6em; animation: ${keyframes`${merge(tada, flip)}`} 5s infinite`;
-        const CardQS = styled.img`height: 20vmin; inline-block; animation: ${keyframes`from {transform: rotate(0deg);} to {transform: rotate(360deg);}`} infinite 3s alternate;`;
-        const CardJD = styled.img`height: 20vmin; inline-block; animation: ${keyframes`from {transform: rotate(360deg);} to {transform: rotate(0deg);}`} infinite 3s alternate;`;
+        // const CardQS = styled.img`height: 20vmin; inline-block; animation: ${keyframes`from {transform: rotate(0deg);} to {transform: rotate(360deg);}`} infinite 3s alternate;`;
+        // const CardJD = styled.img`height: 20vmin; inline-block; animation: ${keyframes`from {transform: rotate(360deg);} to {transform: rotate(0deg);}`} infinite 3s alternate;`;
         // const WavyText = styled.div`font-size: 20px; animation: ${wavifyKeyframes} 1s ease-in-out infinite;`;
         return (
-            <div class="Pinochle">
+            <div class="title-page">
                 <TadaFlip>Nickster Cards™</TadaFlip>
                 <p/><p/>
-                <CardQS src={qos_png} alt="Queen of Spades" />
-                <CardJD src={jod_png} alt="Jack of Diamonds" />
-                <p/><p/>
-                <div class="wavify">
-                    {
-                        ("Loading...").split('').map(function (letter) {
-                            return <span>{letter}</span>
-                        })
-                    }
+                <div class="title-cards" style={{ "white-space": "nowrap" }}>
+                    <img class="title-card-left" src={qos_png} alt="Queen of Spades" />
+                    <img class="title-card-right" src={jod_png} alt="Jack of Diamonds" />
                 </div>
+                <p/><p/>
+                <div class="loading">{ ("Loading...").split('').map(function (a) { return <span>{a}</span> }) }</div>
                 <p/>
                 {/* <p/><p/>
                 <button style={{height: "30px", width: "100px", fontSize: "16px"}} onClick={this.start}>Play!</button> */}
