d24e03427fb82397faf7f805243bc08431fd6185
nicholastwilson <nicholastwilson@hotmail.com>
2023-07-10 15:37:35 -0700

    Setting up game buttons

--- src/pages/games/GamesView.jsx
+++ src/pages/games/GamesView.jsx
@@ -1,10 +1,6 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
 import { ReactComponent as NicksterLogo } from 'assets/nickster_logo.svg';
-import BackButton from 'components/BackButton';
+import PlayingCard from './cards/PlayingCard';
 import './GamesView.scss';
-
 
 export default function GamesView() {
     return (
@@ -12,6 +8,14 @@ export default function GamesView() {
             <NicksterLogo className="games-logo" />
             <div className="games-title">Nickster</div>
             <div className="games-subtitle">Games</div>
+            <div className="game-buttons-container">
+                <div className="pinochle-button">
+                    <div className="game-button">
+                        <div className="card-jack"><PlayingCard suit="Diamonds" rank="Jack" /></div>
+                        <div className="card-queen"><PlayingCard suit="Spades" rank="Queen" /></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
