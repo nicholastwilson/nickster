51390c519f214044d65b4e0d9472234063cae969
nicholastwilson <nicholastwilson@hotmail.com>
2023-10-16 17:03:03 -0700

    Playing card deck, positions, flip actions

--- src/pages/games/cards/pinochle/PinochleGameView.jsx
+++ src/pages/games/cards/pinochle/PinochleGameView.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import { motion} from "framer-motion";
 import _ from "lodash";
@@ -11,58 +11,79 @@ import "./PinochleGameView.scss";
 function PinochleGameView() {
     const { gameID } = useParams();
     // TODO: Retrieve game settings from Supabase
-    const [cards, setCards] = useState([]);
-    const trumpSuit = useMemo(() => _.sample(Object.keys(Suits)), []);
+    const [trumpSuit, setTrumpSuit] = useState(_.sample(Object.keys(Suits)), []);
     const [cardsFlipped, setCardsFlipped] = useState(_.fill(Array(12), false));
-    // const deck = useMemo(() => {
-    //     console.log("Trump = " + trumpSuit);
-    //     const suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
-    //     const ranks = ["Nine", "Jack", "Queen", "King", "Ten", "Ace"];
-    //     const deck = [];
-    //     for(var s = 0; s < suits.length; s++) {
-    //         for(var r = 0; r < ranks.length; r++) {
-    //             deck.push(<PlayingCard key={deck.length} suit={suits[s]} rank={ranks[r]} trump={trumpSuit === suits[s]}  backFaceImage={cardBackImage} />);
-    //         }
-    //     }
-    //     return deck;
-    // }, [trumpSuit]);
-    function handleAddCard() {
-        let suit = _.sample(Object.keys(Suits));
-        let rank = _.sample(["Nine", "Jack", "Queen", "King", "Ten", "Ace"]);
-        const index = cards.length;
-        const handleClick = () => {
-            console.log("Card " + index + " clicked");
-            setCardsFlipped(cardsFlipped.map((c, i) => { return i === index ? !c : c; }));
-        }
-        let newCard = <PlayingCard 
-            key={cards.length} 
-            suit={suit} 
-            rank={rank} 
-            trump={trumpSuit === suit} 
-            trumpColor={"blue"}
-            additionalClasses={`${cardsFlipped[index] ? "pc-flipped" : ""}`} 
-            onClick={handleClick}
-            backFaceImage={cardBackImage}
-        />;
-        // let newCard = _.sample(deck);
-        if(cards.length < 12) {
-            setCards([...cards.slice(0), newCard]);
-        } else {
-            setCards([newCard]);
+    let deck = [];
+    console.log("Trump = " + trumpSuit);
+    const suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+    const ranks = ["Nine", "Jack", "Queen", "King", "Ten", "Ace"];
+    for(var s = 0; s < suits.length; s++) {
+        for(var r = 0; r < ranks.length; r++) {
+            const index = s * ranks.length + r;
+            const handleClick = () => {
+                console.log("Card " + index + " clicked");
+                const newCardsFlipped = cardsFlipped.map((c, i) => { return i === index ? !c : c; });
+                console.log("Cards flipped:\n" + cardsFlipped + " ->\n" + newCardsFlipped);
+                setCardsFlipped(newCardsFlipped);
+                setCards(cards);
+            };
+            deck.push(
+                <div key={index} className="pgv-card" onClick={handleClick} style={{ cursor: "pointer" }}>
+                    <PlayingCard 
+                        key={deck.length} 
+                        suit={suits[s]} 
+                        rank={ranks[r]} 
+                        trump={trumpSuit === suits[s]} 
+                        trumpColor={"blue"}
+                        additionalClasses={`${cardsFlipped[index] ? "pc-flipped" : ""}`}
+                        backFaceImage={cardBackImage}
+                    />
+                </div>
+            );
         }
     }
+    deck = _.shuffle(deck);
+    const [cards, setCards] = useState(deck);
+    // function handleAddCard() {
+    //     let suit = _.sample(Object.keys(Suits));
+    //     let rank = _.sample(["Nine", "Jack", "Queen", "King", "Ten", "Ace"]);
+    //     const index = cards.length < 12 ? cards.length : 0;
+    //     const handleClick = () => {
+    //         console.log("Card " + index + " clicked");
+    //         const newCardsFlipped = cardsFlipped.map((c, i) => { return i === index ? !c : c; });
+    //         console.log("Cards flipped:\n" + cardsFlipped + " ->\n" + newCardsFlipped);
+    //         setCardsFlipped(() => newCardsFlipped);
+    //     }
+    //     let newCard = 
+    //         <div key={index} className="pgv-card" onClick={handleClick} style={{ cursor: "pointer" }}>
+    //             <PlayingCard  
+    //                 suit={suit} 
+    //                 rank={rank} 
+    //                 trump={trumpSuit === suit} 
+    //                 trumpColor={"blue"}
+    //                 additionalClasses={`${cardsFlipped[index] ? "pc-flipped" : ""}`} 
+    //                 backFaceImage={cardBackImage}
+    //             />
+    //         </div>;
+    //     // let newCard = _.sample(deck);
+    //     if(cards.length < 12) {
+    //         setCards([...cards.slice(0), newCard]);
+    //     } else {
+    //         setCards([newCard]);
+    //     }
+    // }
     return (
         <div className="pgv-game-page">
             <div style={{ marginTop: "2vmin" }}>Game ID: {gameID}</div>
-            <div style={{ marginBottom: "2vmin" }}>Trump: {trumpSuit}</div>
+            <div style={{ marginBottom: "2vmin" }} onClick={() => setTrumpSuit(_.sample(Object.keys(Suits)))}>Trump: {trumpSuit}</div>
             {/* Cards */}
             <div className="pgv-cards-container">
-                {cards}
+                {deck}
             </div>
             {/* Add card button */}
-            <motion.button className="pgv-add-card-button" style={{ marginBottom: "2vmin" }} whileTap={{ translate: "0% 5%"}} transition={{ duration: 0.05 }} onClick={handleAddCard}>
+            {/* <motion.button className="pgv-add-card-button" style={{ marginBottom: "2vmin" }} whileTap={{ translate: "0% 5%"}} transition={{ duration: 0.05 }} onClick={handleAddCard}>
                 Add Card
-            </motion.button>
+            </motion.button> */}
         </div>
     );
 }
